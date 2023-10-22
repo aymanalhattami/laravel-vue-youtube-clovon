86389d5863fd8374d36d55653f95eea91b5d1598
aymanalhattami <ayman.m.alhattami@gmail.com>
2023-10-22 17:22:58 +0300

    wip

--- routes/web.php
+++ routes/web.php
@@ -16,3 +16,5 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('{view}', \App\Http\Controllers\ApplicationController::class)->where('view', '(.*)');
