691fb6075f59d6313c537bcfe939399aaedf0531
aymanalhattami <ayman.m.alhattami@gmail.com>
2023-10-22 21:14:46 +0300

    get users

--- resources/js/pages/users/ListUsers.vue
+++ resources/js/pages/users/ListUsers.vue
@@ -1,5 +1,17 @@
 <script setup>
+import {onMounted, ref} from "vue";
 
+    const users = ref([]);
+
+    const getUsers = () => {
+        axios.get('http://laravel-vue-youtube-clovon.test/api/users').then((response) => {
+            users.value = response.data;
+        })
+    }
+
+    onMounted(() => {
+        getUsers();
+    })
 </script>
 
 <template>
@@ -21,6 +33,30 @@
 
     <div class="content">
         <div class="container-fluid">
+            <div class="card-body table-responsive p-0">
+                <table class="table table-hover text-nowrap">
+                    <thead>
+                    <tr>
+                        <th>ID</th>
+                        <th>Name</th>
+                        <th>Email</th>
+<!--                        <th>Role</th>-->
+<!--                        <th>Created at</th>-->
+<!--                        <th>Action</th>-->
+                    </tr>
+                    </thead>
+                    <tbody>
+                    <tr v-for="user in users" :key="user.id">
+                        <td>{{ user.id }}</td>
+                        <td>{{ user.name }}</td>
+                        <td>{{ user.email }}</td>
+<!--                        <td>Admin</td>-->
+<!--                        <td>11-7-2014</td>-->
+<!--                        <td></td>-->
+                    </tr>
+                    </tbody>
+                </table>
+            </div>
         </div>
     </div>
 </template>
